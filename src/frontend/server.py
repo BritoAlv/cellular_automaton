6b592ffe2d34a139d7e7c264064e8dd6d657a691
Javier Lima <javi.limashelby@gmail.com>
2024-09-10 17:22:20 -0400

    feat: Define html site-structure

--- src/frontend/server.py
+++ src/frontend/server.py
@@ -6,5 +6,25 @@ app = Flask(__name__)
 def home():
     return send_file("./public/index.html")
 
+@app.route('/index', methods = ['GET'])
+def index():
+    return send_file("./public/index.html")
+
+@app.route('/armies', methods = ['GET'])
+def armies():
+    return send_file("./public/armies.html")
+
+@app.route('/battle', methods = ['GET'])
+def battle():
+    return send_file("./public/battle.html")
+
+@app.route('/obstacles', methods = ['GET'])
+def obstacles():
+    return send_file("./public/obstacles.html")
+
+@app.route('/dimensions', methods = ['GET'])
+def dimensions():
+    return send_file("./public/dimensions.html")
+
 if __name__ == '__main__':
     app.run(debug=True, port=5050)
